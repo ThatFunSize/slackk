735c640ac2776d2e45c4090aa9b94b10362fd2b8
ThatFunSize <foulofdoom@gmail.com>
2024-07-28 01:09:46 -0600

    used a template to make a sheet and link it, now i gotta do formatting and stuff

--- first.py
+++ first.py
@@ -1,11 +1,3 @@
-import hickle as hkl
-
-#entry_number = hkl.load('entrys')
-mech_options = [{
-					"value": "drivetrain",
-					"text": {
-						"type": "plain_text",
-						"text": "Drivetrain"
-					}
-				}]
-hkl.dump(mech_options, 'mech_cat')
+import gsheet
+valueData = [['jonah'],['Chocolate'],['Chips']]
+gsheet.main(valueData)
